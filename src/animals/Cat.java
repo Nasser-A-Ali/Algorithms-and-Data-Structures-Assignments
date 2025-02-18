4c32d506e45d32a21f7e9627410c911d4dee66e1
Nasser Ali <nasserali60@live.com>
2025-02-18 05:36:08 -0330

    Revert "feat: add "animal shelter manager" assignment"
    This reverts commit d04d2c55d310c5967ca6c256114a53010d6f0bf5.

--- src/animals/Cat.java
+++
@@ -1,7 +0,0 @@
-package animals;
-
-public class Cat extends Animal {
-    public Cat(String name, double arrivalTime) {
-        super(name, arrivalTime);
-    }
-}
--- src/animals/Dog.java
+++
@@ -1,7 +0,0 @@
-package animals;
-
-public class Dog extends Animal {
-    public Dog(String name, double arrivalTime) {
-        super(name, arrivalTime);
-    }
-}
